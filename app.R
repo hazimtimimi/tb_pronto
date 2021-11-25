bd8ff13adb1b7d8d1ce5ee70a13d72861fcbdb51
Hazim Timimi <timimih@who.int>
2021-11-25 17:54:05 +0100

    Split chart code to separate file. Simplified UI to only show two standard charts. Tweaked headers.
    Changed provisional chart to show data for more than one year, with each year as a separate line. If a country reported at different frequencies across the years then convert quarterly data to monthly averages.

--- app.R
+++ app.R
@@ -5,7 +5,7 @@
 # Hazim Timimi, May 2021
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-app_version <- "Version 1.1"
+app_version <- "Version 1.2"
 
 library(shiny)
 library(jsonlite)
@@ -26,19 +26,23 @@ ui <- fluidPage(
 
     # add CSS to colour headings and to prevent printing of the country selector dropdown
     tags$style(HTML("
-    #page_header {padding-top: 10px;}
-    #provisional_heading, #provisional_plus1_heading {
-        color: #4b8f36;
-        padding-left: 5px;}
+    #page_header {
+        padding-top: 10px;
+        padding-left: 20px;}
     #annual_heading {
         color: #1790cf;
         padding-left: 5px;}
+    #provisional_heading {
+        color: #4b8f36;
+        padding-left: 5px;}
+
     @media print {
         #entities, #page_header, #metadata {display: none;}
-                    }")),
+    }")),
 
-    fluidRow(tags$div(style = "padding-left: 20px;",
-                      textOutput(outputId = "page_header"),
+    fluidRow(tags$div(id = "page_header",
+                      HTML("Select from high TB burden countries and other regional priority countries
+                           that reported provisional notifications to WHO<br />"),
                       uiOutput(outputId = "entities"))
              ),
 
@@ -56,18 +60,6 @@ ui <- fluidPage(
         )
     ),
 
-    conditionalPanel(condition = "output.show_dcyear_plus1 == 1",
-
-        fluidRow(
-
-            column(width = 6,
-                   tags$div(style = "padding-left: 20px;"),
-                   textOutput(outputId = "provisional_plus1_heading", container = h3),
-                   echarts4rOutput("prov_plus1_plot")
-                   )
-        )
-    ),
-
     fluidRow(tags$div(style = "padding-left: 20px; padding-right: 20px;",
                       textOutput(outputId = "page_footer"))
     ),
@@ -81,20 +73,21 @@ ui <- fluidPage(
                                   Data collected and published by the
                                   <a href='https://www.who.int/teams/global-tuberculosis-programme/data' target='_blank'>
                             World Health Organization</a>.</i>"))
-
     ))
 
 )
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Backend server code (called each time a new session is initiated)
+# Back end server code (called each time a new session is initiated)
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 server <- function(input, output, session) {
 
-    json_url <- "https://extranet.who.int/tme/generateJSON.asp"
+    # json_url <- "https://extranet.who.int/tme/generateJSON.asp"
+    json_url <- "http://localhost/generateJSON.asp"
 
-    # Get the latest list of counries with provisional data to use in country dropdown
+
+    # Get the latest list of countries with provisional data to use in country dropdown
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     country_list_json <- reactive({
@@ -114,7 +107,7 @@ server <- function(input, output, session) {
 
         already_selected <- input$iso2
 
-        # Create a namedlist for selectInput
+        # Create a named list for selectInput
         country_list <- country_list_json()$countries %>%
             select(iso2, country) %>%
             arrange(country)
@@ -142,298 +135,20 @@ server <- function(input, output, session) {
        return(json)
     })
 
-
-    # Determine whether there are data for dcyear + 1
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    output$show_dcyear_plus1 <- reactive ({
-        req(pdata()$c_newinc_prov)
-
-        nrow_dcyear_plus1 <- pdata()$c_newinc_prov %>%
-            filter(year == country_list_json()$dcyear + 1) %>%
-            nrow()
-
-        return(nrow_dcyear_plus1)
-
-    })
-
-    # Need this to make sure browser can switch elements back on again if hidden
-    outputOptions(output, "show_dcyear_plus1", suspendWhenHidden = FALSE)
-
-
-
-    # Build an Echarts line graph of monthly or quarterly provisional new and relapse cases
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    output$prov_plot <- renderEcharts4r({
-        # Make sure there are data to plot
-        req(pdata()$c_newinc_prov)
-        req(pdata()$c_newinc_year)
-
-        # Store selected country name in variable
-        # to make it easier to display in the chart
-        selected_country <- country_list_json()$countries %>%
-            filter(iso2 == input$iso2)
-
-        # Filter the provisional data to restrict to one year
-        data_to_plot <- pdata()$c_newinc_prov %>%
-            filter(year == country_list_json()$dcyear)
-
-        # Find out whether we have monthly or quarterly data
-        frequency <- as.numeric(data_to_plot$report_frequency)
-        period_prefix <- ifelse(frequency == 71, "q_", "m_")
-        period_name  <- ifelse(frequency == 71, "Quarter", "Month")
-
-
-        # Get the total notification for the previous year and calculate the average monthly or quarterly
-        previous_year <- pdata()$c_newinc_year %>%
-            filter(year == country_list_json()$dcyear - 1) %>%
-            select(c_newinc)
-
-        previous_year_avge <- ifelse(frequency == 71, previous_year/4, previous_year/12) %>% as.numeric()
-
-        # Build the chart
-        # Refer to https://echarts.apache.org/en/option.html for additional properties
-        # noting that instead of using Javascript dot notation to set properties such as yAxis.axisTick.show = true
-        # have to use an R list such as axisTick = list(show = FALSE)
-
-        prov_chart <-  data_to_plot %>%
-
-            # Flip to long format
-            pivot_longer(cols = starts_with(period_prefix),
-                         names_to = "period",
-                         # Add "0?" to the names_prefix regex to remove any leading zeros
-                         names_prefix = paste0(period_prefix, "0?"),
-                         values_to = "c_newinc") %>%
-
-            # Build the chart object
-            e_charts(x = period) %>%
-
-            e_line(serie = c_newinc,
-                   symbolSize = 12) %>%
-
-            e_title(text = selected_country[, "country"])   %>%
-
-            e_tooltip(formatter = JS(paste0("
-                                  function(params){
-                                    return(
-                                    '", period_name, " ' + params.value[0] + ', ",
-                                    country_list_json()$dcyear,
-                                    "' + '<br />Number of cases: ' + params.value[1])
-                                  }
-                                "))) %>%
-
-            e_legend(FALSE) %>%
-
-            e_theme("green") %>%
-
-            # Adjust x axis properties
-            e_x_axis(name = period_name,
-                     nameLocation = 'middle',
-                     nameTextStyle = list(fontSize  = 14,
-                                          padding = 14) ) %>%
-
-            # Make sure large numbers are not truncated in exes labels
-            e_grid(containLabel = TRUE) %>%
-
-            # Add a horizontal line to show previous year's average notification by quarter/month
-            e_mark_line(data = list(yAxis = previous_year_avge),
-                        title = paste0(country_list_json()$dcyear - 1, "\nAverage"),
-
-                        # suppress mouseover tooltip:
-                        silent = TRUE)
-
-        # Before defining the y-axis properties
-        # Calculate the maximum value of all the points and the previous year average
-        max_value <- data_to_plot %>%
-            select(starts_with(period_prefix)) %>%
-            max() %>%
-            max(c(previous_year_avge))
-
-        if (max_value == previous_year_avge) {
-
-            # The automatic y-axis scale will not show the previous year average, so
-            # extend the scale manually
-
-            # Want to calculate the next next convenent highest multiple of 10
-            # one order of magnitude below that of the previous year's average
-            # e.g for 115 we want 120 (next multiple of 10), for 1568 we want 1600 (next multiple of 100), etc.
-
-            order_of_magnitude <- 10^floor(log10(max_value) - 1)
-
-            max_value <- ceiling(max_value/order_of_magnitude) * order_of_magnitude
-
-            prov_chart <- prov_chart %>%
-
-                e_y_axis(axisLine = list(show = FALSE),
-                         axisTick = list(show = FALSE),
-                         # over-ride the scales generated automatically because the
-                         # previous year average doesn't appear if it is higher than any of the
-                         # monthly or quarterly data points
-                         min = 0,
-                         max = max_value)
-
-        } else {
-
-            # The maximum value comes from the data points so let eCharts figure out the scale
-            prov_chart <- prov_chart %>%
-
-                e_y_axis(axisLine = list(show = FALSE),
-                         axisTick = list(show = FALSE))
-
-        }
-
-
-    })
-
-    # Build an Echarts line graph of annual new and relapse cases
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    output$annual_plot <- renderEcharts4r({
-        # Make sure there are data to plot
-        req(pdata()$c_newinc_year)
-        req(pdata()$c_newinc_prov)
-
-        # Store the selected country name to make it easier to display in the chart
-        selected_country <- country_list_json()$countries %>%
-            filter(iso2 == input$iso2)
-
-        # Calculate the total number of cases notified in dcyear
-        c_newinc_latest <- pdata()$c_newinc_prov %>%
-            filter(year == country_list_json()$dcyear) %>%
-            select(-starts_with("report_"), -year) %>%
-            rowSums(na.rm = TRUE)
-
-        # Append the total for dcyear to the annual timeseries
-        c_newinc_annual <- rbind(pdata()$c_newinc_year,
-                                 c(country_list_json()$dcyear, c_newinc_latest ) ) %>%
-
-            # Change the year column to text so that chart is similar to the provisional one
-            mutate(year = as.character(year))
-
-        # Create the chart
-        c_newinc_annual %>%
-
-            e_charts(x = year) %>%
-
-            e_line(serie = c_newinc,
-                   symbolSize = 12) %>%
-
-            e_title(text = selected_country[, "country"])   %>%
-
-            e_legend(FALSE) %>%
-
-            e_theme("blue") %>%
-
-            # Adjust x and y axis properties
-            e_y_axis(axisLine = list(show = FALSE),
-                     axisTick = list(show = FALSE)) %>%
-
-            e_x_axis(name = "Year",
-                     nameLocation = 'middle',
-                     nameTextStyle = list(fontSize  = 14,
-                                          padding = 14)) %>%
-
-            # Make sure large numbers are not truncated in exes labels
-            e_grid(containLabel = TRUE) %>%
-
-
-            e_tooltip(formatter = JS("
-                                  function(params){
-                                    return(
-                                    params.value[0] + '<br />Number of cases: ' + params.value[1])
-                                  }
-                                "))
-
-
-    })
-
-
-    # Build an Echarts line graph of monthly or quarterly provisional new and relapse cases
-    # For dcyear+ 1 if available
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    output$prov_plus1_plot <- renderEcharts4r({
-
-        # Make sure there are data to plot
-        req(pdata()$c_newinc_prov)
-
-        # Store selected country name in variable
-        # to make it easier to display in the chart
-
-        selected_country <- country_list_json()$countries %>%
-            filter(iso2 == input$iso2)
-
-        # Filter the provisional data to restrict to one year
-        data_to_plot <- pdata()$c_newinc_prov %>%
-            filter(year == country_list_json()$dcyear + 1)
-
-        # Find out whether we have monthly or quarterly data
-        frequency <- as.numeric(data_to_plot$report_frequency)
-        period_prefix <- ifelse(frequency == 71, "q_", "m_")
-        period_name  <- ifelse(frequency == 71, "Quarter", "Month")
-
-        # Build the chart
-        prov_chart <-  data_to_plot %>%
-
-            # Flip to long format
-            pivot_longer(cols = starts_with(period_prefix),
-                         names_to = "period",
-                         # Add "0?" to the names_prefix regex to remove any leading zeros
-                         names_prefix = paste0(period_prefix, "0?"),
-                         values_to = "c_newinc") %>%
-
-            # Build the chart object
-            e_charts(x = period) %>%
-
-            e_line(serie = c_newinc,
-                   symbolSize = 12) %>%
-
-            e_title(text = selected_country[, "country"])   %>%
-
-            e_tooltip(formatter = JS(paste0("
-                                  function(params){
-                                    return(
-                                    '", period_name, " ' + params.value[0] + ', ",
-                                    country_list_json()$dcyear + 1,
-                                    "' + '<br />Number of cases: ' + params.value[1])
-                                  }
-                                "))) %>%
-
-            e_legend(FALSE) %>%
-
-            e_theme("green") %>%
-
-            # Adjust x axis properties
-            e_x_axis(name = period_name,
-                     nameLocation = 'middle',
-                     nameTextStyle = list(fontSize  = 14,
-                                          padding = 14) ) %>%
-
-            # Make sure large numbers are not truncated in exes labels
-            e_grid(containLabel = TRUE) %>%
-
-            # Y-axis properies
-            e_y_axis(axisLine = list(show = FALSE),
-                         axisTick = list(show = FALSE))
-
-    })
-
-
+    # Create the charts
+    source("build_charts.r", local = TRUE)
 
     # Put headers and footers in text output components rather than trying to fit them all in the
     # chart titles/subtitles
 
-    output$page_header <- renderText({
-        paste0("Select from high TB burden countries and other regional priority countries that reported",
-               " at least one case in the final reporting period of ",
-               country_list_json()$dcyear,
-               ":")
-    })
-
     output$annual_heading <- renderText({
-        paste0("Number of new and relapse TB cases per year, 2016 - ",
-               country_list_json()$dcyear,
+        # Make sure there are data to plot
+        req(pdata()$dcyear_published)
+
+        paste0("Number of people with new or relapse episodes of TB notified per year, ",
+               # timeseries starts 5 years before the most recent year of publication
+               pdata()$dcyear_published - 5,
+               " - xxxx",
                "*")
     })
 
@@ -442,54 +157,25 @@ server <- function(input, output, session) {
         # Make sure there are data to plot
         req(pdata()$c_newinc_prov)
 
-        # Filter the provisional data to restrict to one year
-        data_to_plot <- pdata()$c_newinc_prov %>%
-            filter(year == country_list_json()$dcyear)
-
         # Find out whether we have monthly or quarterly data
-        frequency <- as.numeric(data_to_plot$report_frequency)
+        frequency <- as.numeric(min(pdata()$c_newinc_prov$report_frequency))
         period_name  <- ifelse(frequency == 71, "quarter", "month")
 
-        paste0("Provisional number of new and relapse TB cases per ",
-               period_name,
-               ", ",
-               country_list_json()$dcyear,
-               "*")
+        paste0("Provisional number of people with new or relapse episodes of TB notified per ",
+               period_name)
     })
 
-    output$provisional_plus1_heading <- renderText({
-        # Make sure there are data to plot
-        req(pdata()$c_newinc_prov)
-
-        # Filter the provisional data to restrict to one year
-        data_to_plot <- pdata()$c_newinc_prov %>%
-            filter(year == country_list_json()$dcyear + 1)
-
-        # Find out whether we have monthly or quarterly data
-        frequency <- as.numeric(data_to_plot$report_frequency)
-        period_name  <- ifelse(frequency == 71, "quarter", "month")
-
-        paste0("Provisional number of new and relapse TB cases per ",
-               period_name,
-               ", ",
-               country_list_json()$dcyear + 1,
-               "*")
-    })
 
     output$page_footer <- renderText({
         # Make sure there are data to plot
         req(pdata()$c_newinc_prov)
 
         # Is reporting comprehensive or partial?
-        coverage <- pdata()$c_newinc_prov %>%
-            filter(year == country_list_json()$dcyear) %>%
-            select(report_coverage) %>%
-            as.numeric()
+        data_to_plot <- pdata()$c_newinc_prov
+
+        report_coverage <- as.numeric(min(data_to_plot$report_coverage))
 
         paste0("* ",
-              country_list_json()$dcyear,
-              " and ",
-              country_list_json()$dcyear + 1,
                # Customise the footnote for China
               ifelse(input$iso2 == "CN",
                      " data for China are for reported pulmonary TB cases published
@@ -502,7 +188,7 @@ server <- function(input, output, session) {
                                      tz = "GMT"),
                              " UTC and subject to change.")
                              ),
-              ifelse(coverage == 35,
+              ifelse(report_coverage == 35,
                      " Does not include data from all reporting units.",
                      "")
              )
---
+++ build_charts.r
@@ -0,0 +1,220 @@
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Build the charts to display annual and provisional monthly or quarterly
+# notifications using the echarts4r package (based on ECharts)
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+# Build an Echarts line graph of annual new and relapse cases
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+output$annual_plot <- renderEcharts4r({
+  # Make sure there are data to plot
+  req(pdata()$c_newinc_year)
+  req(pdata()$c_newinc_prov)
+
+  # Store the selected country name to make it easier to display in the chart
+  selected_country <- country_list_json()$countries %>%
+    filter(iso2 == input$iso2)
+
+  # # Calculate the total number of cases notified in dcyear
+  # c_newinc_latest <- pdata()$c_newinc_prov %>%
+  #     filter(year == country_list_json()$dcyear) %>%
+  #     select(-starts_with("report_"), -year) %>%
+  #     rowSums(na.rm = TRUE)
+  #
+  # # Append the total for dcyear to the annual timeseries
+  # c_newinc_annual <- rbind(pdata()$c_newinc_year,
+  #                          c(country_list_json()$dcyear, c_newinc_latest ) ) %>%
+  #
+
+  c_newinc_annual <- pdata()$c_newinc_year  %>%
+
+    # Change the year column to text so that chart is similar to the provisional one
+    mutate(year = as.character(year))
+
+  # Create the chart
+  c_newinc_annual %>%
+
+    e_charts(x = year) %>%
+
+    e_line(serie = c_newinc,
+           symbolSize = 12) %>%
+
+    e_title(text = selected_country[, "country"])   %>%
+
+    e_legend(FALSE) %>%
+
+    # Adjust x and y axis properties
+    e_y_axis(axisLine = list(show = FALSE),
+             axisTick = list(show = FALSE)) %>%
+
+    e_x_axis(name = "Year",
+             nameLocation = 'middle',
+             nameTextStyle = list(fontSize  = 14,
+                                  padding = 14)) %>%
+
+    # Make sure large numbers are not truncated in exes labels
+    e_grid(containLabel = TRUE) %>%
+
+    e_tooltip(formatter = JS("
+                                  function(params){
+                                    return(
+                                    params.value[0] + '<br />Number notified: ' + params.value[1])
+                                  }
+                                "))
+
+})
+
+
+# Build an Echarts line graph of monthly or quarterly provisional new and relapse cases
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+output$prov_plot <- renderEcharts4r({
+
+  # Make sure there are data to plot
+  req(pdata()$c_newinc_prov)
+
+  # Store selected country name in variable
+  # to make it easier to display in the chart
+  selected_country <- country_list_json()$countries %>%
+    filter(iso2 == input$iso2)
+
+  # Put the provisional data into a variable to make subsequent code easier to read
+  data_to_plot <- pdata()$c_newinc_prov
+
+  # Find out whether we have monthly or quarterly data
+  frequency <- as.numeric(min(data_to_plot$report_frequency))
+  period_prefix <- ifelse(frequency == 71, "q_", "m_")
+  period_name  <- ifelse(frequency == 71, "Quarter", "Month")
+
+  # Check if some years have a different frequency
+  if (min(data_to_plot$report_frequency) != max(data_to_plot$report_frequency)) {
+
+    # Convert any years with quarterly data to monthly equivalent
+    data_to_plot <- data_to_plot %>%
+      mutate(m_01 = ifelse(report_frequency == 71, round(q_1/3), m_01),
+             m_02 = ifelse(report_frequency == 71, round(q_1/3), m_02),
+             m_03 = ifelse(report_frequency == 71, round(q_1/3), m_03),
+
+             m_04 = ifelse(report_frequency == 71, round(q_2/3), m_04),
+             m_05 = ifelse(report_frequency == 71, round(q_2/3), m_05),
+             m_06 = ifelse(report_frequency == 71, round(q_2/3), m_06),
+
+             m_07 = ifelse(report_frequency == 71, round(q_3/3), m_07),
+             m_08 = ifelse(report_frequency == 71, round(q_3/3), m_08),
+             m_09 = ifelse(report_frequency == 71, round(q_3/3), m_09),
+
+             m_10 = ifelse(report_frequency == 71, round(q_4/3), m_10),
+             m_11 = ifelse(report_frequency == 71, round(q_4/3), m_11),
+             m_12 = ifelse(report_frequency == 71, round(q_4/3), m_12),
+
+             year = ifelse(report_frequency == 71, paste(year, "(quarterly averaged as monthly)"), year)
+      )
+
+  }
+
+
+
+  # Get the total notification for 2019 (final pre-pandemic year) and calculate the average monthly or quarterly
+  prepandemic_year <- pdata()$c_newinc_year %>%
+    filter(year == 2019) %>%
+    select(c_newinc)
+
+  prepandemic_year_avge <- ifelse(frequency == 71, prepandemic_year/4, prepandemic_year/12) %>% as.numeric()
+
+  # Build the chart
+  # Refer to https://echarts.apache.org/en/option.html for additional properties
+  # noting that instead of using Javascript dot notation to set properties such as yAxis.axisTick.show = true
+  # have to use an R list such as axisTick = list(show = FALSE)
+
+  prov_chart <-  data_to_plot %>%
+
+    # Flip to long format
+    pivot_longer(cols = starts_with(period_prefix),
+                 names_to = "period",
+                 # Add "0?" to the names_prefix regex to remove any leading zeros
+                 names_prefix = paste0(period_prefix, "0?"),
+                 values_to = "c_newinc") %>%
+
+    select(year, period, c_newinc) %>%
+
+    # Grouping by year makes Echarts show each year as a separate line (named data series)
+    group_by(year) %>%
+
+    # Build the chart object
+    e_charts(x = period) %>%
+
+    e_line(serie = c_newinc,
+           symbolSize = 12) %>%
+
+    e_title(text = selected_country[, "country"])   %>%
+
+    # params.seriesName is the year because the dataframe is grouped by year
+    e_tooltip(formatter = JS(paste0("
+                                  function(params){
+                                    return(
+                                    params.seriesName + ' ' + '", tolower(period_name), " ' + params.value[0]",
+                                  "+ '<br />Number notified: ' + params.value[1] )
+                                  }
+                                "))) %>%
+
+    e_legend(TRUE) %>%
+
+    # Adjust x axis properties
+    e_x_axis(name = period_name,
+             nameLocation = 'middle',
+             nameTextStyle = list(fontSize  = 14,
+                                  padding = 14) ) %>%
+
+    # Make sure large numbers are not truncated in exes labels
+    e_grid(containLabel = TRUE) %>%
+
+    # Add a horizontal line to show pre-pandemic year's average notification by quarter/month
+    e_mark_line(data = list(yAxis = prepandemic_year_avge),
+                title = "2019\nAverage",
+
+                # suppress mouseover tooltip:
+                silent = TRUE)
+
+  # Before defining the y-axis properties
+  # Calculate the maximum value of all the points and the pre-pandemic year average
+  max_value <- data_to_plot %>%
+    select(starts_with(period_prefix)) %>%
+    max(na.rm = TRUE) %>%
+    max(c(prepandemic_year_avge), na.rm = TRUE)
+
+  if (max_value == prepandemic_year_avge) {
+
+    # The automatic y-axis scale will not show the pre-pandemic year average, so
+    # extend the scale manually
+
+    # Want to calculate the next next convenient highest multiple of 10
+    # one order of magnitude below that of the previous year's average
+    # e.g for 115 we want 120 (next multiple of 10), for 1568 we want 1600 (next multiple of 100), etc.
+
+    order_of_magnitude <- 10^floor(log10(max_value) - 1)
+
+    max_value <- ceiling(max_value/order_of_magnitude) * order_of_magnitude
+
+    prov_chart <- prov_chart %>%
+
+      e_y_axis(axisLine = list(show = FALSE),
+               axisTick = list(show = FALSE),
+               # over-ride the scales generated automatically because the
+               # previous year average doesn't appear if it is higher than any of the
+               # monthly or quarterly data points
+               min = 0,
+               max = max_value)
+
+  } else {
+
+    # The maximum value comes from the data points so let eCharts figure out the scale
+    prov_chart <- prov_chart %>%
+
+      e_y_axis(axisLine = list(show = FALSE),
+               axisTick = list(show = FALSE))
+
+  }
+
+
+})
